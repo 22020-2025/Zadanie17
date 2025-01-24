8834cf55b31e053ef6e4e75465ff639036cef5e5
22020-2025 <mateusz.piaskowski333@gmail.com>
2025-01-24 09:09:36 +0100

    Pierwsza rewizja programu.

---
+++ program.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+float cuboid_volume (float a, float b, float h) {
+    return(a*b*h);
+    }
+
+int main(){
+    float a, b, h;
+    printf("Podaj dlugosc podstawy prostopadloscianu: ");
+    scanf("%f",&a);
+
+    printf("Podaj szerokosc podstawy prostopadloscianu: ");
+    scanf("%f",&b);
+
+    printf("Podaj wysokosc podstawy prostopadloscianu: ");
+    scanf("%f",&h);
+
+    printf("\nObjentosc prostopadloscianu: %f\n", cuboid_volume(a, b, h));
+
+    return(0);
+}
